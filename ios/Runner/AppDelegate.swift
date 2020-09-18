386a2d6f9933148cd164c51ffb2fc7a7852daca1
julianusma <jeusmag86@gmail.com>
2020-09-18 00:40:37 -0500

    ios fcm listo

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -13,7 +13,9 @@ import Firebase
     UIApplication.shared.isStatusBarHidden = false
     FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
-
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
